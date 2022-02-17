ebe47350a883d1f2035eb30d62ed59e43225fc7f
Darkrowjkr <90805470+Darkrowjkr@users.noreply.github.com>
2022-02-17 10:53:36 -0600

    Initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+class Config{
+    const SECRET = "loqueseaxdxdxd";
+    
+    const HOST = "localhost";
+    const USER = "root";
+    const BD = "iot";
+    const PASS = "";
+
+}
---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'conexion.php';
+require_once 'jwt.php';
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    if(isset($_GET["user"]) && isset($_GET["pass"])){
+        $c = conexion();
+        $s = $c->prepare("SELECT * FROM users WHERE user = :user AND pass = :pass");
+        $s->bindValue(":user", $_GET["user"]);
+        $s->bindValue(":pass", sha1($_GET["pass"]));
+        $s->execute();
+        $s->setFetchMode(PDO::FETCH_ASSOC);
+        $r = $s->fetch();
+
+        if($r){
+            $t = JWT::create(["user" => $_GET["user"], "rol" => $r['rol']], Config::SECRET);
+            $result = ["login" => "Y", "token" => $t];
+        }else{
+            $result = array("login" => "N", "token" => "Error");
+        }
+        header("http/1.1 200 ok");
+        echo json_encode($result);
+    }else{
+        header("http/1.1 400 bad request");
+    }
+}else{
+    header("http/1.1 405 method not allowed");
+}
